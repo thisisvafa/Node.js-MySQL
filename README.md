f56099733a2bc973072cb18d44e74f911897943b
Mr.Pourtavakkoly <vafa_v17@yahoo.com>
2023-03-02 10:20:47 +0330

    Node.js MySQL Where

--- README.md
+++ README.md
@@ -3,3 +3,4 @@
 2. Node.js MySQL Create Database
 3. Node.js MySQL Create Table
 4. Node.js MySQL Select From
+5. Node.js MySQL Where
---
+++ demo_db_where_s.js
@@ -0,0 +1,30 @@
+var mysql = require('mysql');
+
+var con = mysql.createConnection({
+  host: "localhost",
+  user: "root",
+  password: "",
+  database: "mydb_vafancy"
+});
+
+con.connect(function(err) {
+  if (err) throw err;
+  con.query("SELECT * FROM customers WHERE address LIKE 'S%'", function (err, result) {
+    if (err) throw err;
+    console.log(result);
+  });
+});
+
+var adr = 'Mountain 21';
+var sql = 'SELECT * FROM customers WHERE address = ' + mysql.escape(adr);
+con.query(sql, function (err, result) {
+  if (err) throw err;
+  console.log(result);
+});
+
+var adr = 'Mountain 21';
+var sql = 'SELECT * FROM customers WHERE address = ?';
+con.query(sql, [adr], function (err, result) {
+  if (err) throw err;
+  console.log(result);
+});
